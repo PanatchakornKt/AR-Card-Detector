ef396d22e58995847eda3862a640cb8afa98dbc8
Panatchakorn Worachotchanapat <panatchakorn@Panatchakorns-MacBook-Pro.local>
2021-07-16 09:19:29 +0700

    create 3D image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import OpenCamera from "@/components/OpenCamera";
 import OpenCameraIOS from "@/components/OpenCamiOS";
+import ThreeDimisionImage from "@/components/3DImage";
 
 export default function Home() {
   return (
     <>
       <title>AR Card</title>
       {/* <OpenCameraIOS /> */}
-      <OpenCamera />
+      {/* <OpenCamera /> */}
+      <ThreeDimisionImage />
     </>
   );
 }
